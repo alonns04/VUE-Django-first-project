63c7b1f1f5202f16204effad329028615d30b556
alonns04 <claudiogabrielalonso@gmail.com>
2024-03-09 14:57:11 -0300

    search logrado, archivo con composition API

--- drf-vue/tienda/tienda/settings.py
+++ drf-vue/tienda/tienda/settings.py
@@ -158,3 +158,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
